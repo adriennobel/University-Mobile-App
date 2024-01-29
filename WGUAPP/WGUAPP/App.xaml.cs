d1a9c803f471f890aad8fc54b5b3f685b7c9633c
whitneybantar <misschal200@gmail.com>
2024-01-28 20:46:23 -0500

    First commit: created MAUI template app project

---
+++ WGUAPP/WGUAPP/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WGUAPP
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ WGUAPP/WGUAPP/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WGUAPP
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
